79aa206902fa8ee621289f13b6d5b3721a354559
Jumanazar04 <yusupovjumanazar004@gmail.com>
2023-09-21 22:42:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const form = document.querySelector('.forma'),
+input = document.querySelector('.input'),
+btn = document.querySelector('.btn'),
+result = document.querySelector('.result'),
+span = document.querySelector('.hidden')
+item = document.querySelector('.item');
+
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    if(input.value){
+       const item = document.createElement('div');
+       item.classList.add('item')
+       
+       const text = document.createElement('p')
+       text.classList.add('item-text');
+       item.appendChild(text);
+       text.textContent = input.value;
+
+       const itemChild = document.createElement('div')
+       itemChild.classList.add('item-child')
+       item.appendChild(itemChild)
+
+       const editBtn = document.createElement('button')
+       editBtn.classList.add('edit-btn')
+       editBtn.classList.add('btn')
+       itemChild.appendChild(editBtn)
+       editBtn.textContent = 'Edit'
+
+       const deleteBtn = document.createElement('button')
+       deleteBtn.classList.add('delete-btn')
+       itemChild.appendChild(deleteBtn)
+       deleteBtn.classList.add('btn')
+       deleteBtn.textContent = 'Delete'
+
+       result.appendChild(item)
+
+       form.reset()
+    }else{
+        span.classList.remove('hidden');
+        setTimeout(()=>{
+            span.classList.add('hidden');
+        }, 2500)
+        
+    }
+    
+})
+
+
